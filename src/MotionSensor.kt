e57c9cef7e5e80a95da04c9ad6a4c7e277c149ad
MJARuijs <mja.ruijs@gmail.com>
2018-05-11 13:25:11 +0200

    Add Runnable

--- src/MotionSensor.kt
+++ src/MotionSensor.kt
@@ -1,25 +1,31 @@
 import com.pi4j.io.gpio.GpioPinDigitalInput
 
-class MotionSensor(val sensorPin: GpioPinDigitalInput, val callback: MotionSensorCallback) {
+class MotionSensor(private val sensorPin: GpioPinDigitalInput, private val callback: MotionSensorCallback) {
 
     private var lastMovementDetected = 0L
     var movementDetected = false
 
-    var stateChanged = false
-
     fun update() {
         val currentTime = System.currentTimeMillis()
-        stateChanged = false
 
-        if (!movementDetected && sensorPin.isHigh) {
-            movementDetected = true
-            callback.onStateChanged(true)
+        if (sensorPin.isHigh) {
+            lastMovementDetected = currentTime
+
+            if (!movementDetected) {
+                movementDetected = true
+                callback.onStateChanged(true)
+            }
+        } else if (movementDetected && sensorPin.isLow) {
+            if (currentTime > lastMovementDetected + LIGHT_OFF_DELAY) {
+                movementDetected = false
+                callback.onStateChanged(false)
+            }
         }
 
     }
 
     companion object {
-        private const val LIGHT_OFF_DELAY = 10
+        private const val LIGHT_OFF_DELAY = 5000
     }
 
     interface MotionSensorCallback {
