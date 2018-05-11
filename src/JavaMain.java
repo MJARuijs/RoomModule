2cc52de3f6af882839d6c482ebe9d5a6716ef44e
MJARuijs <mja.ruijs@gmail.com>
2018-05-11 12:19:24 +0200

    Add Runnable

--- src/JavaMain.java
+++ src/JavaMain.java
@@ -1,12 +1,7 @@
-import com.pi4j.io.gpio.*;
-
 public class JavaMain {
 
     public static void main(String[] args) {
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        Main.main();
     }
 
 }
