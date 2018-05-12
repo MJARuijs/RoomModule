12c4c3515c85c210b864f19462a0d7b622200b07
MJARuijs <mja.ruijs@gmail.com>
2018-05-12 18:19:54 +0200

    Add Runnable

--- src/HardwareManager.kt
+++ src/HardwareManager.kt
@@ -29,7 +29,7 @@ class HardwareManager {
         if (response == "COMMAND_NOT_SUPPORTED" || response == "ERROR") {
             return getConfiguration()
         }
-
+        println(response)
         return response
     }
 }
