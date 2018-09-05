fa76afe15396671241a9296e83348d10c2d8b237
MJARuijs <mja.ruijs@gmail.com>
2018-09-05 13:51:41 +0200

    Implement lighting

--- src/MotionSensor.kt
+++ src/MotionSensor.kt
@@ -36,7 +36,7 @@ class MotionSensor(private val sensorPin: GpioPinDigitalInput, private val power
                 Thread {
                     println("CHECKING")
                     powerPin.setState(false)
-                    Thread.sleep(10)
+                    Thread.sleep(50)
                     powerPin.setState(true)
                     println(sensorPin.isHigh)
 //                    val runTime = Runtime.getRuntime()
